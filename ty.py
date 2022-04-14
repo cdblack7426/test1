6fc591753c844f91a606fb874ec42fd15035586f
cdblack7426 <103653428+cdblack7426@users.noreply.github.com>
2022-04-14 17:27:36 +0300

    Update ty.py

--- ty.py
+++ ty.py
@@ -1,2 +1,26 @@
-sfvsv
-sdcdscsd
+'''
+Application example using build() + return
+==========================================
+
+An application can be built if you return a widget on build(), or if you set
+self.root.
+'''
+
+import kivy
+
+kivy.require('1.0.7')
+
+from kivy.app import App
+from kivy.uix.button import Button
+
+
+class MyApp(App):
+
+    def build(self):
+        # return a Button() as a root widget
+        return Button(text='hello AlexP')
+
+
+MyApp().run()
+# if __name__ == '__main__':
+# MyApp().run()
